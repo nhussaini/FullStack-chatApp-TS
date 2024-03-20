e816ec83c5c905d7a660b559549e51f602541096
nhussaini <nasrullah_hussaini@yahoo.com>
2024-03-19 22:14:54 -0400

    add message controller

--- backend/app.ts
+++ backend/app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.routes';
 import connectToMongoDB from './db/connectToMongoDB';
 import messageRoutes from './routes/message.routes';
@@ -16,6 +17,7 @@ const PORT = process.env.PORT || 5000;
 // });
 
 app.use(express.json()); //to parse incoming requests with JSON payloads (from req.body)
+app.use(cookieParser()); // to parse the incoming cookies from req.cookies
 //auth routes
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
--- backend/controllers/message.controller.ts
+++ backend/controllers/message.controller.ts
@@ -1,5 +1,38 @@
 import { Request, Response } from 'express';
+import Conversation from '../models/conversation.model';
+import Message from '../models/message.model';
+// Define a custom Request type by extending the Request type from Express
+interface CustomRequest extends Request {
+  user?: any; // Add the user property to the Request type
+}
+export const sendMessage = async (req: CustomRequest, res: Response) => {
+  try {
+    const { message } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user?._id;
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    });
 
-export const sendMessage = async (req: Request, res: Response) => {
-  console.log('message sent');
+    if (!conversation) {
+      conversation = await Conversation.create({
+        participants: [senderId, receiverId],
+      });
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      message,
+    });
+    if (newMessage) {
+      conversation.messages.push(newMessage._id);
+    }
+
+    await Promise.all([conversation.save(), newMessage.save()]);
+    res.status(201).json(newMessage);
+  } catch (error: any) {
+    console.log('Error in protectRoute middleware: ', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 };
