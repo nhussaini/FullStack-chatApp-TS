b6ac12e69b4609ac1f4c545f4f44b78f275dfabb
nhussaini <nasrullah_hussaini@yahoo.com>
2024-03-22 12:25:18 -0400

    add home, login and signup pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,11 @@
 import './App.css';
+import Login from './pages/login/Login';
 
 function App() {
   return (
-    <>
-      <button className="btn btn-primary">One</button>
-      <button className="btn btn-secondary">Two</button>
-      <button className="btn btn-accent btn-outline">Three</button>
-    </>
+    <div className="p-4 h-screen flex items-center justify-center">
+      <Login />
+    </div>
   );
 }
 
