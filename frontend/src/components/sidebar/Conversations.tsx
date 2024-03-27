a24a0320468ae8d46fd78cd14342e32af40e20f3
nhussaini <nasrullah_hussaini@yahoo.com>
2024-03-26 20:56:34 -0400

    create conversations for sidebar

---
+++ frontend/src/components/sidebar/Conversations.tsx
@@ -0,0 +1,15 @@
+import Conversation from './Conversation';
+
+const Conversations = () => {
+  return (
+    <div className="py-2 flex flex-col overdlow-auto">
+      <Conversation />
+      <Conversation />
+      <Conversation />
+      <Conversation />
+      <Conversation />
+      <Conversation />
+    </div>
+  );
+};
+export default Conversations;
--- frontend/src/components/sidebar/Sidebar.tsx
+++ frontend/src/components/sidebar/Sidebar.tsx
@@ -1,12 +1,13 @@
+import Conversations from './Conversations';
 import SearchInput from './SearchInput';
 
 const Sidebar = () => {
   return (
     <div>
       <SearchInput />
+      <Conversations />
       <div className="divider px-3"></div>
-      {/* <Conversations />
-      <LogoutButton /> */}
+      {/* <LogoutButton />  */}
     </div>
   );
 };
