6203d1d38d32cb4e143d31223869053f9c357345
nhussaini <nasrullah_hussaini@yahoo.com>
2024-03-26 20:23:13 -0400

    create searchinput for sidebar

---
+++ frontend/src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,13 @@
+import SearchInput from './SearchInput';
+
+const Sidebar = () => {
+  return (
+    <div>
+      <SearchInput />
+      <div className="divider px-3"></div>
+      {/* <Conversations />
+      <LogoutButton /> */}
+    </div>
+  );
+};
+export default Sidebar;
